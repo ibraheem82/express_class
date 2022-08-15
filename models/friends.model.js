61c7a9744368b191885b7e4bb7316d95a6d1efb1
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-15 17:16:26 +0100

    friends array was created.

---
+++ models/friends.model.js
@@ -0,0 +1,71 @@
+const friends =  [{
+    id: 0,
+    name: 'Ibraheem Omikunle'
+},
+
+{
+    id: 1,
+    name: 'John Williams'
+},
+
+{
+    id: 2,
+    name: 'Kara waltham'
+},
+
+{
+    id: 3,
+    name: 'Dorathy Charles'
+},
+
+{
+    id: 4,
+    name: 'Adeyinka Bolaji'
+},
+
+{
+    id: 5,
+    name: 'Adebayo Sodik'
+},
+
+
+{
+    id: 6,
+    name: 'Ayomide Mobolaji'
+},
+
+
+{
+    id: 7,
+    name: 'Razaq Olawale'
+},
+
+
+{
+    id: 8,
+    name: 'Damilola Rukayat'
+},
+
+
+{
+    id: 9,
+    name: 'Tope Boldlinks'
+},
+
+{
+    id: 10,
+    name: 'Ololade Asake'
+},
+
+
+{
+    id: 11,
+    name: 'Mufti Menk'
+},
+
+
+{
+    id: 3,
+    name: 'Sudais Rahman'
+}
+]
