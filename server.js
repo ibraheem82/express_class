b4f7621bcc82f3b1c227a0afcb0a6cbbe271312d
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-15 16:29:24 +0100

    route was moved to another place.

--- server.js
+++ server.js
@@ -19,11 +19,97 @@ const friends =  [{
 {
     id: 3,
     name: 'Dorathy Charles'
+},
+
+{
+    id: 4,
+    name: 'Adeyinka Bolaji'
+},
+
+{
+    id: 5,
+    name: 'Adebayo Sodik'
+},
+
+
+{
+    id: 6,
+    name: 'Ayomide Mobolaji'
+},
+
+
+{
+    id: 7,
+    name: 'Razaq Olawale'
+},
+
+
+{
+    id: 8,
+    name: 'Damilola Rukayat'
+},
+
+
+{
+    id: 9,
+    name: 'Tope Boldlinks'
+},
+
+{
+    id: 10,
+    name: 'Ololade Asake'
+},
+
+
+{
+    id: 11,
+    name: 'Mufti Menk'
+},
+
+
+{
+    id: 3,
+    name: 'Sudais Rahman'
 }
 ]
-// app.get('/', (req, res) => {
-//     res.send('Omikunle')
-// })
+// * Middleware in express
+/*
+    ! the next() must always be called when using middleware.
+*/
+app.use((req, res, next) => {
+    const start = Date.now()
+    next();
+
+    // * will check the diffrence in time
+    // * Total of milliseconds between when the request was made and time the request was sent back.
+    const delta  = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+
+});
+
+// * Set to json
+app.use(express.json());
+
+// * Add Friend
+app.post('/friends', (req, res) => {
+    if(!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friends name'
+        })
+    }
+
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    };
+    friends.push(newFriend);
+
+    res.send(newFriend);
+});
+
+
+
+app.get('/', )
 
 
 app.get('/friends', (req, res) => {
