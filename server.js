61c1360c194b691a4ef8a77e7afff19ef9b1dfe8
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-15 17:10:35 +0100

    routes was well structured.

--- server.js
+++ server.js
@@ -93,45 +93,11 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // * Add Friend
-app.post('/friends', (req, res) => {
-    if(!req.body.name) {
-        return res.status(400).json({
-            error: 'Missing friends name'
-        })
-    }
-
-    const newFriend = {
-        name: req.body.name,
-        id: friends.length
-    };
-    friends.push(newFriend);
-
-    res.send(newFriend);
-});
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend)
 
 
-
-
-
-app.get('/friends', (req, res) => {
-    // res.send(friends);
-    res.json(friends);
-});
-
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend)
-        // res.json(friend)
-    } else {
-        res.status(404).json({
-
-            error: "Friend does not exist"
-            
-        });
-    }
-})
 
 app.get('/messages', messagesController.getMessages)
 app.post('/messages', messagesController.postMessage)
