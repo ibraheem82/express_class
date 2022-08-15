1cc459f38cdac36f22ae713bc051407ac7ba9dd3
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-15 17:14:09 +0100

    friends array was removed.

--- server.js
+++ server.js
@@ -3,77 +3,7 @@ const friendsController = require('./controllers/friends.controller');
 const messagesController = require('./controllers/messages.controller');
 const app  = express();
 const PORT = 5000;
-const friends =  [{
-    id: 0,
-    name: 'Ibraheem Omikunle'
-},
 
-{
-    id: 1,
-    name: 'John Williams'
-},
-
-{
-    id: 2,
-    name: 'Kara waltham'
-},
-
-{
-    id: 3,
-    name: 'Dorathy Charles'
-},
-
-{
-    id: 4,
-    name: 'Adeyinka Bolaji'
-},
-
-{
-    id: 5,
-    name: 'Adebayo Sodik'
-},
-
-
-{
-    id: 6,
-    name: 'Ayomide Mobolaji'
-},
-
-
-{
-    id: 7,
-    name: 'Razaq Olawale'
-},
-
-
-{
-    id: 8,
-    name: 'Damilola Rukayat'
-},
-
-
-{
-    id: 9,
-    name: 'Tope Boldlinks'
-},
-
-{
-    id: 10,
-    name: 'Ololade Asake'
-},
-
-
-{
-    id: 11,
-    name: 'Mufti Menk'
-},
-
-
-{
-    id: 3,
-    name: 'Sudais Rahman'
-}
-]
 // * Middleware in express
 /*
     ! the next() must always be called when using middleware.
